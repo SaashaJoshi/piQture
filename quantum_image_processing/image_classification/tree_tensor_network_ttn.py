c1ae549606c3b7c0c13fd8dc121908a43de63856
SaashaJoshi <saashajoshi@uvic.ca>
2023-01-20 22:03:35 -0800

    add tree tensor network implementation with COBYLA optimizer

--- quantum_image_processing/image_classification/tree_tensor_network_ttn.py
+++ quantum_image_processing/image_classification/tree_tensor_network_ttn.py
@@ -4,7 +4,8 @@ from qiskit.utils import algorithm_globals
 from qiskit.circuit import QuantumCircuit, ParameterVector
 from qiskit_machine_learning.neural_networks import EstimatorQNN
 from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier
-from qiskit.algorithms.optimizers import ADAM
+from qiskit.algorithms.optimizers import ADAM, COBYLA
+from qiskit.quantum_info import SparsePauliOp
 from quantum_image_processing.data_loader.mnist_data_loader import load_mnist_data
 
 
@@ -104,6 +105,13 @@ if __name__ == "__main__":
 
     print(train_img.shape, train_labels.shape)
 
+    train_labels = np.array(train_labels)
+    for i in range(len(train_labels)):
+        if train_labels[i] == 9:
+            train_labels[i] = -1
+        elif train_labels[i] == 2:
+            train_labels[i] = 1
+
     # flatten_img_dim = train_img[0].reshape(-1, 4)
     # params_list = flatten_img_dim.tolist()[0]
     # # print(params_list)
@@ -119,9 +127,11 @@ if __name__ == "__main__":
     circ.decompose().decompose().draw("mpl")
     # plt.show()
 
-    from qiskit.quantum_info import SparsePauliOp
-
-    observable = SparsePauliOp.from_list([("Z" * 4, 1)])
+    # observable = SparsePauliOp.from_list([("Z" * 4, 1)])
+    observable = SparsePauliOp.from_list([("Z" + "I" * 3, 1)])
+    # Shouldn't this be I*3 + Z (IIIZ) because we just measure the last qubit
+    # Shit it would be the exact opposite since qiskit ordering of qubit is diff.
+    # But would it be opposite really?
 
     def callback_graph(weights, obj_func_eval):
         # clear_output(wait=True)
@@ -132,6 +142,7 @@ if __name__ == "__main__":
         plt.plot(range(len(objective_func_vals)), objective_func_vals)
         plt.savefig("graph.pdf")
         plt.show()
+
 
     estimator_qnn = EstimatorQNN(
         circuit=circ,
@@ -147,7 +158,7 @@ if __name__ == "__main__":
 
     estimator_classifier = NeuralNetworkClassifier(
         estimator_qnn,
-        optimizer=ADAM(maxiter=10),
+        optimizer=COBYLA(maxiter=40),
         callback=callback_graph,
     )
 
@@ -156,4 +167,17 @@ if __name__ == "__main__":
     estimator_classifier.score(train_img, np.array(train_labels))
 
     test_predict = estimator_classifier.predict(test_img)
-    print(test_label, test_predict)
+    # print(test_label, test_predict)
+
+    test_label = np.array(test_label)
+    for i in range(len(test_label)):
+        if test_label[i] == 9:
+            test_label[i] = -1
+        elif test_label[i] == 2:
+            test_label[i] = 1
+
+    count = 0
+    for i in range(len(test_predict)):
+        if test_predict[i] == test_label[i]:
+            count += 1
+    print(count)
