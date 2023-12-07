412fded0617532d843d90ccc802380d5c0a2a13e
SaashaJoshi <saashajoshi08@gmail.com>
2023-12-06 22:35:11 -0800

    100 percent coverage with ttn and two_qubit_unitary tests

--- tests/models/tensor_networks/test_ttn.py
+++ tests/models/tensor_networks/test_ttn.py
@@ -14,42 +14,57 @@ from quantum_image_processing.gates.two_qubit_unitary import TwoQubitUnitary
 @pytest.fixture(name="ttn_simple_circuit")
 def ttn_simple_circuit_fixture():
     """Fixture to replicate a real simple two-qubit unitary block."""
-    def _ttn_simple_circuit(img_dims, parameter_vector):
+
+    def _ttn_simple_circuit(img_dims, parameter_vector, parameterization):
         test_circuit = QuantumCircuit(int(math.prod(img_dims)))
+
+        parameterization_callable = {
+            "real_simple": [TwoQubitUnitary().real_simple_block, 2],
+            "real_general": [TwoQubitUnitary().real_general_block, 6],
+            "complex_general": [TwoQubitUnitary().complex_general_block, 15],
+        }
+
+        mapper = parameterization_callable[parameterization]
         if math.prod(img_dims) == 2:
             test_circuit.compose(
-                TwoQubitUnitary().real_simple_block(parameter_vector[:2])[0],
+                mapper[0](parameter_vector[: mapper[1]])[0],
                 qubits=[0, 1],
                 inplace=True,
             )
+            test_circuit.ry(parameter_vector[mapper[1]], len(test_circuit.qubits) - 1)
         elif math.prod(img_dims) == 3:
             test_circuit.compose(
-                TwoQubitUnitary().real_simple_block(parameter_vector[:2])[0],
+                mapper[0](parameter_vector[: mapper[1]])[0],
                 qubits=[0, 1],
                 inplace=True,
             )
             test_circuit.compose(
-                TwoQubitUnitary().real_simple_block(parameter_vector[2:4])[0],
+                mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
                 qubits=[1, 2],
                 inplace=True,
             )
+            test_circuit.ry(
+                parameter_vector[2 * mapper[1]], len(test_circuit.qubits) - 1
+            )
         elif math.prod(img_dims) == 4:
             test_circuit.compose(
-                TwoQubitUnitary().real_simple_block(parameter_vector[:2])[0],
+                mapper[0](parameter_vector[: mapper[1]])[0],
                 qubits=[0, 1],
                 inplace=True,
             )
             test_circuit.compose(
-                TwoQubitUnitary().real_simple_block(parameter_vector[2:4])[0],
+                mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
                 qubits=[2, 3],
                 inplace=True,
             )
             test_circuit.compose(
-                TwoQubitUnitary().real_simple_block(parameter_vector[4:6])[0],
+                mapper[0](parameter_vector[2 * mapper[1] : 3 * mapper[1]])[0],
                 qubits=[1, 3],
                 inplace=True,
             )
-        test_circuit.ry(parameter_vector[-1], len(test_circuit.qubits) - 1)
+            test_circuit.ry(
+                parameter_vector[3 * mapper[1]], len(test_circuit.qubits) - 1
+            )
         return test_circuit
 
     return _ttn_simple_circuit
@@ -84,7 +99,7 @@ class TestTTN:
     )
     def test_ttn_simple(self, img_dims, complex_structure):
         # pylint: disable=line-too-long
-        """Tests the ttn_backbone mehtod call via the ttn_simple function."""
+        """Tests the ttn_backbone method call via the ttn_simple function."""
         with mock.patch(
             "quantum_image_processing.models.tensor_network_circuits.ttn.TTN.ttn_backbone"
         ) as mock_ttn_simple:
@@ -114,21 +129,38 @@ class TestTTN:
                 )
 
     @pytest.mark.parametrize(
-        "img_dims, complex_structure",
-        [((1, 3), False), ((2, 2), False), ((1, 2), False)],
+        "img_dims, complex_structure, parameterization",
+        [
+            ((1, 3), False, "real_general"),
+            ((2, 2), False, "real_simple"),
+            ((1, 2), True, "complex_general"),
+        ],
     )
-    def test_ttn_backbone(self, img_dims, complex_structure, ttn_simple_circuit):
-        """
-        Tests the ttn_backbone circuit.
-        Currently, tests only for simple real parameterization, i.e. complex_structure = False.
-        """
-        parameter_vector = ParameterVector(
-            "test", length=2 * int(math.prod(img_dims)) - 1
+    def test_ttn_backbone(
+        self, img_dims, complex_structure, parameterization, ttn_simple_circuit
+    ):
+        """Tests the ttn_backbone circuit with real and complex parameterization."""
+        parameterization_mapper = {
+            "real_simple": [
+                ParameterVector("test", length=2 * int(math.prod(img_dims)) - 1),
+                TwoQubitUnitary().simple_parameterization,
+            ],
+            "real_general": [
+                ParameterVector("test", 6 * int(math.prod(img_dims)) - 1),
+                TwoQubitUnitary().general_parameterization,
+            ],
+            "complex_general": [
+                ParameterVector("test", 15 * int(math.prod(img_dims)) - 1),
+                TwoQubitUnitary().general_parameterization,
+            ],
+        }
+
+        test_circuit = ttn_simple_circuit(
+            img_dims, parameterization_mapper[parameterization][0], parameterization
         )
-        test_circuit = ttn_simple_circuit(img_dims, parameter_vector)
         circuit = TTN(img_dims).ttn_backbone(
-            TwoQubitUnitary().simple_parameterization,
-            parameter_vector,
+            parameterization_mapper[parameterization][1],
+            parameterization_mapper[parameterization][0],
             complex_structure,
         )
         assert circuit.data == test_circuit.data
