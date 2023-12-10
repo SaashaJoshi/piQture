a9885c2dccfa4652cc4342491850d5fb44b3eeca
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2023-12-09 23:40:19 -0800

    Add MPS tensor network (#24)
    * add mps tensor network with unit tests.
    
    * make TTN child class of MPS and remove lint erros. however, we do ignore some duplicate in three files - mps, ttn, and mera.

--- tests/models/tensor_networks/test_mps.py
+++ tests/models/tensor_networks/test_mps.py
@@ -1,20 +1,22 @@
-"""Unit test for TTN class"""
+"""Unit test for MPS class"""
 from __future__ import annotations
 
 import math
+import re
 from unittest import mock
 import pytest
+from pytest import raises
 from qiskit.circuit import QuantumCircuit, ParameterVector
-from quantum_image_processing.models.tensor_network_circuits.ttn import TTN
+from quantum_image_processing.models.tensor_network_circuits.mps import MPS
 from quantum_image_processing.gates.two_qubit_unitary import TwoQubitUnitary
 
 
-@pytest.fixture(name="ttn_circuit")
-def ttn_circuit_fixture():
+@pytest.fixture(name="mps_circuit")
+def mps_circuit_fixture():
     """Fixture to replicate a real simple two-qubit unitary block."""
 
     # pylint: disable=duplicate-code
-    def _ttn_circuit(img_dims, parameter_vector, parameterization):
+    def _mps_circuit(img_dims, parameter_vector, parameterization):
         test_circuit = QuantumCircuit(int(math.prod(img_dims)))
 
         parameterization_callable = {
@@ -24,91 +26,84 @@ def ttn_circuit_fixture():
         }
 
         mapper = parameterization_callable[parameterization]
-        if math.prod(img_dims) == 2:
+        if math.prod(img_dims) >= 2:
             test_circuit.compose(
                 mapper[0](parameter_vector[: mapper[1]])[0],
                 qubits=[0, 1],
                 inplace=True,
             )
-            test_circuit.ry(parameter_vector[mapper[1]], len(test_circuit.qubits) - 1)
-        elif math.prod(img_dims) == 3:
-            test_circuit.compose(
-                mapper[0](parameter_vector[: mapper[1]])[0],
-                qubits=[0, 1],
-                inplace=True,
-            )
-            test_circuit.compose(
-                mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
-                qubits=[1, 2],
-                inplace=True,
-            )
-            test_circuit.ry(
-                parameter_vector[2 * mapper[1]], len(test_circuit.qubits) - 1
-            )
-        elif math.prod(img_dims) == 4:
-            test_circuit.compose(
-                mapper[0](parameter_vector[: mapper[1]])[0],
-                qubits=[0, 1],
-                inplace=True,
-            )
-            test_circuit.compose(
-                mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
-                qubits=[2, 3],
-                inplace=True,
-            )
-            test_circuit.compose(
-                mapper[0](parameter_vector[2 * mapper[1] : 3 * mapper[1]])[0],
-                qubits=[1, 3],
-                inplace=True,
-            )
-            test_circuit.ry(
-                parameter_vector[3 * mapper[1]], len(test_circuit.qubits) - 1
-            )
+            if math.prod(img_dims) >= 3:
+                test_circuit.compose(
+                    mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
+                    qubits=[1, 2],
+                    inplace=True,
+                )
+                if math.prod(img_dims) >= 4:
+                    test_circuit.compose(
+                        mapper[0](parameter_vector[2 * mapper[1] : 3 * mapper[1]])[0],
+                        qubits=[2, 3],
+                        inplace=True,
+                    )
         return test_circuit
 
-    return _ttn_circuit
+    return _mps_circuit
 
 
-class TestTTN:
-    """Tests for TTN class"""
+class TestMPS:
+    """Tests for MPS class"""
+
+    @pytest.mark.parametrize("img_dims", [({"abc", "def"}), (2, 1.5), (None, None)])
+    def test_img_dims(self, img_dims):
+        """Tests the type of img_dims input."""
+        with raises(
+            TypeError,
+            match=re.escape("Input img_dims must be of the type tuple[int, int]."),
+        ):
+            _ = MPS(img_dims)
+
+    @pytest.mark.parametrize("img_dims", [(-3, 1), (2, 0)])
+    def test_num_qubits(self, img_dims):
+        """Tests the product of img_dims."""
+        with raises(ValueError, match="Image dimensions cannot be zero or negative."):
+            _ = MPS(img_dims)
 
     @pytest.mark.parametrize("img_dims", [(2, 4)])
     def test_circuit_property(self, img_dims):
-        """Tests the TTN circuit initialization."""
+        """Tests the MPS circuit initialization."""
         test_circuit = QuantumCircuit(math.prod(img_dims))
-        assert test_circuit.data == TTN(img_dims).circuit.data
+        assert test_circuit.data == MPS(img_dims).circuit.data
 
     @pytest.mark.parametrize(
         "img_dims, complex_structure", [((2, 2), False), ((4, 5), True)]
     )
-    def test_ttn_simple(self, img_dims, complex_structure):
+    def test_mps_simple(self, img_dims, complex_structure):
         # pylint: disable=line-too-long
-        """Tests the ttn_backbone method call via the ttn_simple function."""
+        """Tests the mps_backbone method call via the mps_simple function."""
         with mock.patch(
-            "quantum_image_processing.models.tensor_network_circuits.ttn.TTN.ttn_backbone"
-        ) as mock_ttn_simple:
+            "quantum_image_processing.models.tensor_network_circuits.mps.MPS.mps_backbone"
+        ) as mock_mps_simple:
             with mock.patch(
                 "quantum_image_processing.gates.two_qubit_unitary.TwoQubitUnitary.simple_parameterization"
             ) as simple_parameterization:
-                _ = TTN(img_dims).ttn_simple(complex_structure)
-                mock_ttn_simple.assert_called_once_with(
+                _ = MPS(img_dims).mps_simple(complex_structure)
+                mock_mps_simple.assert_called_once_with(
                     simple_parameterization, mock.ANY, complex_structure
                 )
 
     @pytest.mark.parametrize(
         "img_dims, complex_structure", [((2, 2), False), ((4, 5), True)]
     )
-    def test_ttn_general(self, img_dims, complex_structure):
+    def test_mps_general(self, img_dims, complex_structure):
         # pylint: disable=line-too-long
-        """Tests the ttn_backbone method call via the ttn_general function."""
+        """Tests the mps_backbone method call via the mps_general function."""
         with mock.patch(
-            "quantum_image_processing.models.tensor_network_circuits.ttn.TTN.ttn_backbone"
-        ) as mock_ttn_general:
+            "quantum_image_processing.models.tensor_network_circuits.mps.MPS.mps_backbone"
+        ) as mock_mps_general:
             with mock.patch(
                 "quantum_image_processing.gates.two_qubit_unitary.TwoQubitUnitary.general_parameterization"
             ) as general_parameterization:
-                _ = TTN(img_dims).ttn_general(complex_structure)
-                mock_ttn_general.assert_called_once_with(
+                _ = MPS(img_dims).mps_general(complex_structure)
+                mock_mps_general.assert_called_once_with(
                     general_parameterization, mock.ANY, complex_structure
                 )
 
@@ -126,29 +121,29 @@ class TestTTN:
             ((1, 4), True, "complex_general"),
         ],
     )
-    def test_ttn_backbone(
-        self, img_dims, complex_structure, parameterization, ttn_circuit
+    def test_mps_backbone(
+        self, img_dims, complex_structure, parameterization, mps_circuit
     ):
-        """Tests the ttn_backbone circuit with real and complex parameterization."""
+        """Tests the mps_backbone circuit with real and complex parameterization."""
         parameterization_mapper = {
             "real_simple": [
-                ParameterVector("test", length=2 * int(math.prod(img_dims)) - 1),
+                ParameterVector("test", 2 * int(math.prod(img_dims)) - 2),
                 TwoQubitUnitary().simple_parameterization,
             ],
             "real_general": [
-                ParameterVector("test", 6 * int(math.prod(img_dims)) - 1),
+                ParameterVector("test", 6 * int(math.prod(img_dims)) - 2),
                 TwoQubitUnitary().general_parameterization,
             ],
             "complex_general": [
-                ParameterVector("test", 15 * int(math.prod(img_dims)) - 1),
+                ParameterVector("test", 15 * int(math.prod(img_dims)) - 2),
                 TwoQubitUnitary().general_parameterization,
             ],
         }
 
-        test_circuit = ttn_circuit(
+        test_circuit = mps_circuit(
             img_dims, parameterization_mapper[parameterization][0], parameterization
         )
-        circuit = TTN(img_dims).ttn_backbone(
+        circuit = MPS(img_dims).mps_backbone(
             parameterization_mapper[parameterization][1],
             parameterization_mapper[parameterization][0],
             complex_structure,
