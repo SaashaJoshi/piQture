25234b64570881cb45f82c0dc62eba938e9a2963
SaashaJoshi <saashajoshi08@gmail.com>
2023-12-14 13:27:13 -0800

    update tests for tensor network circuits after changes made in PR #27

--- tests/models/tensor_networks/test_mps.py
+++ tests/models/tensor_networks/test_mps.py
@@ -1,8 +1,5 @@
 """Unit test for MPS class"""
 from __future__ import annotations
-
-import math
-import re
 from unittest import mock
 import pytest
 from pytest import raises
@@ -15,22 +12,22 @@ from quantum_image_processing.gates.two_qubit_unitary import TwoQubitUnitary
 def parameterization_mapper_fixture():
     """Fixture for parameterization mapper dictionary."""
 
-    def _mapper(img_dims):
+    def _mapper(num_qubits):
         parameterization_mapper = {
             "real_simple": [
-                ParameterVector("test", 2 * int(math.prod(img_dims)) - 1),
+                ParameterVector("test", 2 * num_qubits - 1),
                 TwoQubitUnitary().simple_parameterization,
             ],
             "complex_simple": [
-                ParameterVector("test", 2 * int(math.prod(img_dims)) - 1),
+                ParameterVector("test", 2 * num_qubits - 1),
                 TwoQubitUnitary().simple_parameterization,
             ],
             "real_general": [
-                ParameterVector("test", 6 * int(math.prod(img_dims)) - 1),
+                ParameterVector("test", 6 * num_qubits - 1),
                 TwoQubitUnitary().general_parameterization,
             ],
             "complex_general": [
-                ParameterVector("test", 15 * int(math.prod(img_dims)) - 1),
+                ParameterVector("test", 15 * num_qubits - 1),
                 TwoQubitUnitary().general_parameterization,
             ],
         }
@@ -44,8 +41,8 @@ def mps_circuit_fixture():
     """Fixture to replicate a real simple two-qubit unitary block."""
 
     # pylint: disable=duplicate-code
-    def _mps_circuit(img_dims, parameter_vector, parameterization):
-        test_circuit = QuantumCircuit(int(math.prod(img_dims)))
+    def _mps_circuit(num_qubits, parameter_vector, parameterization):
+        test_circuit = QuantumCircuit(num_qubits)
 
         parameterization_callable = {
             "real_simple": [TwoQubitUnitary().real_simple_block, 2],
@@ -55,19 +52,19 @@ def mps_circuit_fixture():
         }
 
         mapper = parameterization_callable[parameterization]
-        if math.prod(img_dims) >= 2:
+        if num_qubits >= 2:
             test_circuit.compose(
                 mapper[0](parameter_vector[: mapper[1]])[0],
                 qubits=[0, 1],
                 inplace=True,
             )
-            if math.prod(img_dims) >= 3:
+            if num_qubits >= 3:
                 test_circuit.compose(
                     mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
                     qubits=[1, 2],
                     inplace=True,
                 )
-                if math.prod(img_dims) >= 4:
+                if num_qubits >= 4:
                     test_circuit.compose(
                         mapper[0](parameter_vector[2 * mapper[1] : 3 * mapper[1]])[0],
                         qubits=[2, 3],
@@ -81,37 +78,32 @@ def mps_circuit_fixture():
 class TestMPS:
     """Tests for MPS class"""
 
-    @pytest.mark.parametrize("img_dims", [(2, 4)])
-    def test_repr(self, img_dims):
+    @pytest.mark.parametrize("num_qubits", [8])
+    def test_repr(self, num_qubits):
         """Tests the representation of the MPS class."""
-        test_representation = f"MatrixProductState(img_dims={img_dims})"
-        assert test_representation == repr(MPS(img_dims))
+        test_representation = f"MatrixProductState(num_qubits={num_qubits})"
+        assert test_representation == repr(MPS(num_qubits))
 
-    @pytest.mark.parametrize("img_dims", [({"abc", "def"}), (2, 1.5), (None, None)])
-    def test_img_dims(self, img_dims):
-        """Tests the type of img_dims input."""
-        with raises(
-            TypeError,
-            match=re.escape("Input img_dims must be of the type tuple[int, int]."),
-        ):
-            _ = MPS(img_dims)
+    @pytest.mark.parametrize("num_qubits", [None, {"abc"}, "pqr"])
+    def test_type_num_qubits(self, num_qubits):
+        """Tests the type of input num_qubit."""
+        with raises(TypeError, match="Input num_qubits must be of the type int."):
+            _ = MPS(num_qubits)
 
-    @pytest.mark.parametrize("img_dims", [(-3, 1), (2, 0)])
-    def test_num_qubits(self, img_dims):
-        """Tests the product of img_dims."""
-        with raises(ValueError, match="Image dimensions cannot be zero or negative."):
-            _ = MPS(img_dims)
+    @pytest.mark.parametrize("num_qubits", [0, -20])
+    def test_value_num_qubits(self, num_qubits):
+        """Tests value of input num_qubits."""
+        with raises(ValueError, match="Number of qubits cannot be zero or negative."):
+            _ = MPS(num_qubits)
 
-    @pytest.mark.parametrize("img_dims", [(2, 4)])
-    def test_circuit_property(self, img_dims):
+    @pytest.mark.parametrize("num_qubits", [8])
+    def test_circuit_property(self, num_qubits):
         """Tests the MPS circuit initialization."""
-        test_circuit = QuantumCircuit(math.prod(img_dims))
-        assert test_circuit.data == MPS(img_dims).circuit.data
+        test_circuit = QuantumCircuit(num_qubits)
+        assert test_circuit.data == MPS(num_qubits).circuit.data
 
-    @pytest.mark.parametrize(
-        "img_dims, complex_structure", [((2, 2), False), ((4, 5), True)]
-    )
-    def test_mps_simple(self, img_dims, complex_structure):
+    @pytest.mark.parametrize("num_qubits, complex_structure", [(4, False), (12, True)])
+    def test_mps_simple(self, num_qubits, complex_structure):
         # pylint: disable=line-too-long
         """Tests the mps_backbone method call via the mps_simple function."""
         with mock.patch(
@@ -120,15 +112,13 @@ class TestMPS:
             with mock.patch(
                 "quantum_image_processing.gates.two_qubit_unitary.TwoQubitUnitary.simple_parameterization"
             ) as simple_parameterization:
-                _ = MPS(img_dims).mps_simple(complex_structure)
+                _ = MPS(num_qubits).mps_simple(complex_structure)
                 mock_mps_simple.assert_called_once_with(
                     simple_parameterization, mock.ANY, complex_structure
                 )
 
-    @pytest.mark.parametrize(
-        "img_dims, complex_structure", [((2, 2), False), ((4, 5), True)]
-    )
-    def test_mps_general(self, img_dims, complex_structure):
+    @pytest.mark.parametrize("num_qubits, complex_structure", [(4, False), (20, True)])
+    def test_mps_general(self, num_qubits, complex_structure):
         # pylint: disable=line-too-long
         """Tests the mps_backbone method call via the mps_general function."""
         with mock.patch(
@@ -137,29 +127,29 @@ class TestMPS:
             with mock.patch(
                 "quantum_image_processing.gates.two_qubit_unitary.TwoQubitUnitary.general_parameterization"
             ) as general_parameterization:
-                _ = MPS(img_dims).mps_general(complex_structure)
+                _ = MPS(num_qubits).mps_general(complex_structure)
                 mock_mps_general.assert_called_once_with(
                     general_parameterization, mock.ANY, complex_structure
                 )
 
     @pytest.mark.parametrize(
-        "img_dims, complex_structure, parameterization",
+        "num_qubits, complex_structure, parameterization",
         [
-            ((1, 2), False, "real_general"),
-            ((1, 3), False, "real_general"),
-            ((1, 4), False, "real_general"),
-            ((2, 1), False, "real_simple"),
-            ((1, 3), False, "real_simple"),
-            ((2, 2), False, "real_simple"),
-            ((1, 2), True, "complex_simple"),
-            ((1, 2), True, "complex_general"),
-            ((1, 3), True, "complex_general"),
-            ((1, 4), True, "complex_general"),
+            (2, False, "real_general"),
+            (3, False, "real_general"),
+            (4, False, "real_general"),
+            (2, False, "real_simple"),
+            (3, False, "real_simple"),
+            (4, False, "real_simple"),
+            (2, True, "complex_simple"),
+            (2, True, "complex_general"),
+            (3, True, "complex_general"),
+            (4, True, "complex_general"),
         ],
     )
     def test_mps_backbone(
         self,
-        img_dims,
+        num_qubits,
         complex_structure,
         parameterization,
         mps_circuit,
@@ -167,11 +157,11 @@ class TestMPS:
     ):
         # pylint: disable=too-many-arguments
         """Tests the mps_backbone circuit with real and complex parameterization."""
-        parameterization_mapper = parameterization_mapper(img_dims)
+        parameterization_mapper = parameterization_mapper(num_qubits)
         test_circuit = mps_circuit(
-            img_dims, parameterization_mapper[parameterization][0], parameterization
+            num_qubits, parameterization_mapper[parameterization][0], parameterization
         )
-        circuit = MPS(img_dims).mps_backbone(
+        circuit = MPS(num_qubits).mps_backbone(
             parameterization_mapper[parameterization][1],
             parameterization_mapper[parameterization][0],
             complex_structure,
