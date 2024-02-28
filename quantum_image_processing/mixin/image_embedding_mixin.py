17192b7bca41fd222762267e9946e11f8b088d85
SaashaJoshi <saashajoshi08@gmail.com>
2024-02-27 23:32:11 -0800

    add copyright

--- quantum_image_processing/mixin/image_embedding_mixin.py
+++ quantum_image_processing/mixin/image_embedding_mixin.py
@@ -1,3 +1,13 @@
+# (C) Copyright SaashaJoshi 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 """Mixin class for Image Embedding methods"""
 
 from __future__ import annotations
