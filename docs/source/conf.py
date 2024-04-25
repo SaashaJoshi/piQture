afdeb4684bfbb4c28914ea995a1f410fb6a3b419
SaashaJoshi <saashajoshi08@gmail.com>
2024-04-24 20:23:20 -0700

    lint changes to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
+"""Configuration file for the Sphinx documentation builder."""
+
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -6,23 +7,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'piQture'
-copyright = '2024, Saasha Joshi'
-author = 'Saasha Joshi'
-release = '0.0.1'
+PROJECT = "piQture"
+COPYRIGHT_NOTICE = "2024, Saasha Joshi"
+AUTHOR = "Saasha Joshi"
+RELEASE = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+HTML_THEME = "alabaster"
+html_static_path = ["_static"]
