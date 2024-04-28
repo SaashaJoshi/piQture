9b8ff297d9e5a12a06b94509dfe6edaf5144e963
SaashaJoshi <saashajoshi08@gmail.com>
2024-04-28 00:16:04 -0700

    made edits to the wrapper function. added few docstrings

--- piqture/data_loader/mnist_data_loader.py
+++ piqture/data_loader/mnist_data_loader.py
@@ -10,21 +10,31 @@
 
 """Data Loader for MNIST images"""
 
+from __future__ import annotations
+from typing import Union
 import torch.utils.data
 import torchvision
 from torchvision import datasets
 
 
-def load_mnist_dataset(labels=list(range(10)), batch_size=None, size=2, normalize_min=0, normalize_max=1):
+def load_mnist_dataset(
+    labels: list = list(range(10)),
+    batch_size: int = 64,
+    size: Union[int, tuple] = 2,
+    normalize_min: int = 0,
+    normalize_max: int = 1,
+):
     """
     Loads MNIST dataset from PyTorch using DataLoader.
 
     Args:
         labels (list): List of desired labels.
-        batch_size (int, optional): Batch size for the DataLoader. Defaults to 64.
-        size (int or tuple, optional): Size to which images will be resized. If an integer is provided,
-            images will be resized to a square of that size. If a tuple of two integers is provided,
-            images will be resized to the specified height and width. Defaults to 2.
+        batch_size (int, optional): Batch size for the dataset. Defaults to 64.
+        size (int or tuple, optional): Size to which images will be resized. Defaults to 2.
+            If an integer is provided, images will be resized to a
+            square of that size.
+            If a tuple of integers is provided, images will be resized to
+            the specified height and width.
         normalize_min (float, optional): Minimum value for normalization. Defaults to 0.
         normalize_max (float, optional): Maximum value for normalization. Defaults to 1.
 
@@ -32,14 +42,23 @@ def load_mnist_dataset(labels=list(range(10)), batch_size=None, size=2, normaliz
         Train and Test DataLoader objects.
     """
 
-    def normalize_data(x, min_val, max_val):
-        return (x - min_val) / (max_val - min_val)
+    def normalize_data(x, normalize_min, normalize_max):
+        """Normalizes data to a range [min, max]."""
+        return (x - normalize_min) / (normalize_max - normalize_min)
 
-    mnist_transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Resize(size) if isinstance(size, int) else torchvision.transforms.Resize(size[::-1]),
-        torchvision.transforms.Lambda(lambda x: normalize_data(x, normalize_min, normalize_max))
-    ])
+    mnist_transform = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
+            (
+                torchvision.transforms.Resize(size)
+                if isinstance(size, int)
+                else torchvision.transforms.Resize(size[::-1])
+            ),
+            torchvision.transforms.Lambda(
+                lambda x: normalize_data(x, normalize_min, normalize_max)
+            ),
+        ]
+    )
 
     mnist_train = datasets.MNIST(
         root="data/mnist_data",
@@ -49,10 +68,7 @@ def load_mnist_dataset(labels=list(range(10)), batch_size=None, size=2, normaliz
     )
 
     mnist_test = datasets.MNIST(
-        root="data/mnist_data",
-        train=False,
-        download=True,
-        transform=mnist_transform
+        root="data/mnist_data", train=False, download=True, transform=mnist_transform
     )
 
     if batch_size is not None:
@@ -65,7 +81,7 @@ def load_mnist_dataset(labels=list(range(10)), batch_size=None, size=2, normaliz
 
         test_dataloader = torch.utils.data.DataLoader(
             dataset=mnist_test,
-            batch_size=70000-batch_size,
+            batch_size=70000 - batch_size,
             shuffle=False,
             collate_fn=lambda batch: collate_fn(batch, labels),
         )
@@ -75,9 +91,9 @@ def load_mnist_dataset(labels=list(range(10)), batch_size=None, size=2, normaliz
     return mnist_train, mnist_test
 
 
-def collate_fn(batch, labels):
+def collate_fn(batch, labels: list):
     """
-    Batches the data according to the defined function.
+    Batches the images wrt the provided labels.
     """
     new_batch = []
     for img, label in batch:
