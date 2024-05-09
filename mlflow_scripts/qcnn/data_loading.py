f064ea0e93b66d20dbf2d9e2ea3d3f3e79a94e8a
SaashaJoshi <saashajoshi08@gmail.com>
2024-05-09 00:24:39 -0700

    edit load_mnist_dataset argument order

--- mlflow_scripts/qcnn/data_loading.py
+++ mlflow_scripts/qcnn/data_loading.py
@@ -1,17 +1,28 @@
 import mlflow
 import click
-from piqture.data_loader.mnist_data_loader import load_mnist_dataset
+import piqture
+from piqture.data_loader import load_mnist_dataset
+
 
 @click.command()
-@click.option("--labels", default=None, help="list of desired labels.")
+@click.option("--img_size", default=28, help="size to which images will be resized.")
 @click.option("--batch_size", default=None, help="batch size for the dataset.")
-@click.option("--img_size", default=2, help="size to which images will be resized.")
-@click.option("--norm_min", default=0, help="minimum value for normalization.")
-@click.option("--norm_max", default=1, help="maximum value for normalization.")
-def load_data(labels, batch_size, img_size, norm_min, norm_max):
+@click.option("--labels", default=None, help="list of desired labels.")
+@click.option("--norm_min", default=None, help="minimum value for normalization.")
+@click.option("--norm_max", default=None, help="maximum value for normalization.")
+def load_data(img_size, batch_size, labels, norm_min, norm_max):
     with mlflow.start_run():
-        train_dataloader, test_dataloader = load_mnist_dataset(labels, batch_size, img_size, norm_min, norm_max)
-        print(train_dataloader)
+        train_dataloader, test_dataloader = load_mnist_dataset(
+            img_size=img_size,
+            batch_size=batch_size,
+            labels=labels,
+            normalize_min=norm_min,
+            normalize_max=norm_max,
+        )
+
+        for batch in train_dataloader:
+            if batch:
+                print(batch)
 
 
 if __name__ == "__main__":
