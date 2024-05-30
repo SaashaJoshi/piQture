b4b873603257b1cdb32833e4acc858e882d1548f
SaashaJoshi <saashajoshi08@gmail.com>
2024-05-29 19:39:47 -0700

    make correcttions

--- tests/sample_circuit.py
+++ tests/sample_circuit.py
@@ -6,10 +6,9 @@ from qiskit.circuit import QuantumCircuit
 
 
 def main():
-    num_qubits = 4
-    circuit = QuantumCircuit(2)
+    circuit = QuantumCircuit(2, 2)
     circuit.measure([0, 1], [0, 1])
-    # circuit.draw("mpl")
+    circuit.draw("mpl")
     # plt.show()
 
     # provider = IBMProvider(token="IBMQ_API_TOKEN")
