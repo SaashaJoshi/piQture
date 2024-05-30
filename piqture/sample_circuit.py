bc23e271e6224cae67f680b2f0a4059ca006e03b
SaashaJoshi <saashajoshi08@gmail.com>
2024-05-29 19:09:24 -0700

    corect file path and workflow

--- piqture/sample_circuit.py
+++ piqture/sample_circuit.py
@@ -1,13 +1,16 @@
 from tensor_network_circuits.ttn import TTN
 from qiskit_ibm_provider import IBMProvider
 # import matplotlib.pyplot as plt
+import click
 
-if __name__ == "__main__":
+
+def main():
     num_qubits = 4
     circuit = TTN(num_qubits).ttn_simple(complex_structure=False)
     # circuit.draw("mpl")
     # plt.show()
 
+    # provider = IBMProvider(token="IBMQ_API_TOKEN")
     provider = IBMProvider()
     print(provider.backend)
     backend = provider.get_backend("ibm_qasm_simulator")
@@ -15,3 +18,7 @@ if __name__ == "__main__":
     result = job.result()
     counts = result.get_counts()
     print(counts)
+
+
+if __name__ == "__main__":
+    main()
